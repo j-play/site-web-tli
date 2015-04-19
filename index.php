e5ac861214835da09dd19a8871d3d9f4ab758990
jonathanplay <jonathan.play@hotmail.fr>
2015-04-19 14:42:06 +0200

    commit

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
 	// Gestion de Routing
 	/*if (isset($_GET['action']) && file_exists(_CTRL_.'action/'.str_replace('.', '', $_GET['action']).'.php'))
     	include(_CTRL_.'action/'.$_GET['action'].'.php');*/
-
+	$get = '';
 	if(isset($_GET['page']) && file_exists(_CTRL_.str_replace('.', '', $_GET['page']).'.php')){
     	include_once(_CTRL_.$_GET['page'].'.php');	
+		$get =$_GET['page'];
     }
 	else {
     	include_once(_CTRL_.'index.php');
@@ -16,7 +17,8 @@
     
     // Affichages
 
-	if($_GET['page'] != 'calculatrice'){
+	
+	if($get != 'calculatrice'){
 		
 		$smarty->display(_TPL_ . 'header.tpl');
 
